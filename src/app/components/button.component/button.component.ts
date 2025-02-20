5167c80e80b5d336e46123ff1c52f478b460dcb4
Mr_roob <mohammadshahrash4@gmail.com>
2025-02-20 10:25:43 +0330

    app angular

---
+++ src/app/components/button.component/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'karvrz',
+    templateUrl: 'button.component.html',
+    styleUrl:'button.component.css'
+})
+
+export class buttonComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
